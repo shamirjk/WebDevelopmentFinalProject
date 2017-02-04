a0531ce04b72ef4297a30a27146c5705852bf803
Shamir Kritzler <shamirjk@gmail.com>
2017-02-04 14:39:16 +0200

    Fixing Tables

--- fill_tables.php
+++ fill_tables.php
@@ -4,9 +4,12 @@
     //fill users table
     if (mysqli_query($conn,
         "INSERT INTO tbl_user_207 (id, user, name, email) VALUES
-        (\"283774651\",\"admin\",\"בר קריצלר\",\"ar.bar@gmail.com\"),
+        (\"283774651\",\"admin\",\"בר קריצלר\",\"bar.bar@gmail.com\"),
         (\"890337461\",\"coach\",\"אבי כהן\",\"avi.cohen@gmail.com\"),
-        (\"657448391\",\"client\",\"שי שי\",\"shi.shi@gmail.com\")
+        (\"890337461\",\"coach\",\"משה אשכנזי\",\"moshe.ash@gmail.com\"),
+        (\"890337461\",\"coach\",\"יובל מנדלוביץ\",\"yoval.mendel@gmail.com\"),
+        (\"657448391\",\"client\",\"שי לוי\",\"shai.shai@gmail.com\"),
+        (\"890337461\",\"client\",\"משה גרוס\",\"moshe.moshe@gmail.com\"),
         "))
     {
         echo "<p>Table tbl_client_207 filled successfully</p>";
