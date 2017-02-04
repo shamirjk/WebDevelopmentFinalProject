2841c8b37bec565b75b9697ff84dcb513028eb2a
juriksun <juriksun@gmail.com>
2017-02-04 16:56:15 +0200

    Work on java script log in file

--- includes/log_action.js
+++ includes/log_action.js
@@ -2,8 +2,22 @@ $(document).ready(function () {
     $("form").submit(function () {
         var userId = $("input[name=userId]").val();
         var user = $("select[name=user]").val();
+        var dataString = "user=" + user + "&id=" + userId;
         console.log(userId + "  " + user);
-
-        $("form").attr("action","index.html?user=" + user + "&id=" + userId);
+        $.ajax({
+            type: "POST",
+            url: "user_varification.php",
+            data: dataString,
+            cache: true,
+            success: function(data){
+                if(data == "true"){
+                    $("form").attr("action","index.html?user=" + user + "&id=" + userId);
+                } else if(data == "false"){
+                    $("wrong").html("נתונים שגוים");
+                } else{
+                    $("wrong").html(data);
+                }
+            }
+        });
     });
 });
