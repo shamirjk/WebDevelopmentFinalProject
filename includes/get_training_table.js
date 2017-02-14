be82f94f43fca5d1ff2a73b52bd3699b03ba8cc6
juriksun <juriksun@gmail.com>
2017-02-14 21:02:40 +0200

    google map location
    Signed-off-by: juriksun <juriksun@gmail.com>

--- includes/get_training_table.js
+++ includes/get_training_table.js
@@ -12,63 +12,78 @@ $(document).ready( function () {
     });
 });
 
-var openCloseTraining= function() {
-    var allTrainingTr =$("tr[id^='tr']").each(function () {
+var openCloseTraining = function () {
+    var allTrainingTr = $("tr[id^='tr']").each(function () {
         console.log("in function each");
         $(this).click(function () {
             var nextTrThis = $(this).next();
-            console.log("lalala");
-            if(nextTrThis.hasClass('addClosed')){
-                console.log("in click open");
+            //initMap(nextTrThis.find(".addiAddress"));
+            jasonMap(nextTrThis.find('.addiAddress'));
+            if (nextTrThis.hasClass('addClosed')) {
                 nextTrThis.removeClass('addClosed').addClass('addOpened');
             } else {
                 nextTrThis.removeClass('addOpened').addClass('addClosed');
             }
-            allTrainingTr.each(function(){
+            allTrainingTr.each(function () {
                 var nextTr = $(this).next();
-                if(!nextTrThis.is(nextTr)) {
-                    console.log("in closed");
+                if (!nextTrThis.is(nextTr)) {
                     if (nextTr.hasClass('addOpened')) {
                         nextTr.removeClass('addOpened').addClass('addClosed');
                     }
                 }
             });
-
         });
     });
 };
 
-/*
-var openCloseTraining = function(){
-    var trTraining = $(".list");
-        trTraining.each(function () {
-            var tr =$(this);
-            tr.click(function (){
-                if($(this).attr('class') == "list"){
-                    trTraining.each(function () {
-                        $(this).removeClass("list").addClass("notFull");
-                        console.log($(this).attr('class'));
-                    });
-                    $(this).removeClass("notFull").addClass("full");
 
-                    $(".addTraining").css("display","none");
-                    console.log(tr.attr('class'));
-                    $("#hamburger").parent().removeClass("lead").addClass();
-                    $("#back").parent().addClass("lead");
-                }
-                // <ul class="hambBack">
-                //     <li><a id="back" href="#"></a></li>
-                //     <li class="lead"><a href="#" id="hamburger"></a></li>
-                //     </ul>
+var jasonMap = function(trOb) {
+    //mapGoogle
 
+    console.log(trOb.find(".mapGoogle").attr('class'));
+
+    var jasonPath = (trOb.children('p').eq(1).html() + "," + trOb.children('p').eq(2).html()).split(" ").join("+");
+
+    jasonPath = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+        jasonPath + "&key=AIzaSyBIxztqzoBiEWON80fImWSA6WY1D7yx2GY";
+
+    console.log(jasonPath);
+
+    $.getJSON(jasonPath, function (data) {
+        console.log(data.status);
+        if (data.status == "OK") {
+
+            var location = data.results[0];//.geometry.location;
+
+            console.log(typeof location.lat);
+            console.log(typeof location.lng);
+            initMap(location,trOb.find('.mapGoogle')[0])
+        }
+    });
+
+};
+function initMap(location,trOb) {
+    var map = new google.maps.Map(trOb, {
+        center: {lat: location.geometry.location.lat , lng: location.geometry.location.lng},
+        zoom: 15
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails({
+        placeId: location.place_id
+    }, function(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
             });
-        });
-};*/
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
+    });
+}
 
-// $("#hamburger, .overlay").click(function(){
-//     $("header").toggleClass(function(){
-//         if($(this).is(".closed")){
-//             return "opened";
-//         }else return "closed";
-//     });
-// });
